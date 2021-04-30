b31395d6229a8d1ef12b99494676b585c2adf0ec
daneelsan <dansando@protonmail.com>
2021-04-30 00:00:06 -0500

    E004 - Animating the Backbuffer

--- code/build.bat
+++ code/build.bat
@@ -7,6 +7,6 @@ pushd ..\build
 del *.pdb > NUL 2> NUL
 
 echo %cd%
-cl -Zi ..\code\win32_handmade.c User32.lib Gdi32.lib
+cl -FAsc -Zi ..\code\win32_handmade.c User32.lib Gdi32.lib
 
 popd
--- code/win32_handmade.c
+++ code/win32_handmade.c
@@ -1,25 +1,52 @@
 #pragma warning(push, 3)
 #include <windows.h>
 #pragma warning(pop)
+#include <stdint.h>
 
 #define internal static
 #define global_variable static
 #define local_persist static
 
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
 // @NOTE: `running` is global for now
 global_variable BOOL running;
 global_variable BITMAPINFO bitmapInfo;
 global_variable VOID *bitmapMemory;
-global_variable HBITMAP bitmapHandle;
-global_variable HDC bitmapDeviceContext;
+global_variable WORD bytesPerPixel = 4;
+global_variable int bitmapWidth;
+global_variable int bitmapHeight;
+
+internal void renderStuff(void) {
+  local_persist int offset = 0;
+
+  int width = bitmapWidth;
+  int height = bitmapHeight;
+  int pitch = width * bytesPerPixel;
+  u8 *row = (u8 *)bitmapMemory;
+
+  for (int y = 0; y < height; ++y) {
+    u32 *pixel = (u32 *)row;
+    for (int x = 0; x < width; ++x) {
+      *pixel = (u32)(x + y + offset);
+      pixel += 1;
+    }
+    row += pitch;
+  }
+
+  ++offset;
+}
+
 
 LRESULT CALLBACK Win32MainWindowCallback(_In_ HWND windowHandle,
                                          _In_ UINT messageID,
                                          _In_ WPARAM wParam,
                                          _In_ LPARAM lParam);
 internal void Win32ResizeDIBSection(int width, int height);
-internal void Win32UpdateWindow(HDC deviceContext, int x, int y, int width,
-                                int height);
+internal void Win32UpdateWindow(HDC deviceContext, RECT windowRect);
 
 int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance,
                    _In_ LPSTR commandLine, _In_ int commandShow) {
@@ -57,18 +84,24 @@ int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance,
   }
 
   MSG msg = {0};
-  BOOL msgResult;
   running = TRUE;
 
   while (running) {
-    msgResult = GetMessageA(&msg, NULL, 0, 0);
-    if (msgResult == -1) {
-      OutputDebugStringA("Error:GetMessageA\n");
-      return 0;
-    } else {
+    while (PeekMessageA(&msg, windowHandle, 0, 0, PM_REMOVE)) {
+      if (msg.message == WM_QUIT) {
+        running = FALSE;
+      }
       TranslateMessage(&msg);
       DispatchMessageA(&msg);
     }
+
+    renderStuff();
+
+    HDC deviceContext = GetDC(windowHandle);
+    RECT clientRect;
+    GetClientRect(windowHandle, &clientRect);
+    Win32UpdateWindow(deviceContext, clientRect);
+    ReleaseDC(windowHandle, deviceContext);
   }
 
   // MessageBoxA(NULL, "This is Handmade Hero", "Handmade Hero",
@@ -123,11 +156,14 @@ LRESULT CALLBACK Win32MainWindowCallback(_In_ HWND windowHandle,
       PAINTSTRUCT paint;
       HDC deviceContext = BeginPaint(windowHandle, &paint);
 
-      int x = paint.rcPaint.left;
+      /*int x = paint.rcPaint.left;
       int y = paint.rcPaint.top;
       int width = paint.rcPaint.right - x;
-      int height = paint.rcPaint.bottom - y;
-      Win32UpdateWindow(deviceContext, x, y, width, height);
+      int height = paint.rcPaint.bottom - y;*/
+
+      RECT clientRect;
+      GetClientRect(windowHandle, &clientRect);
+      Win32UpdateWindow(deviceContext, clientRect);
 
       TextOutA(deviceContext, 0, 0, "Hello, Windows!", 15);
 
@@ -149,36 +185,44 @@ LRESULT CALLBACK Win32MainWindowCallback(_In_ HWND windowHandle,
 
   return result;
 }
-
 internal void Win32ResizeDIBSection(int width, int height) {
-  // @TODO: Bullet proof this
-  // Maybe don't free first, free after, then free first if that fails
+  if (bitmapMemory != NULL) {
+    VirtualFree(bitmapMemory, 0, MEM_RELEASE);
+  }
 
-  if (bitmapHandle != NULL) {
-    DeleteObject(bitmapHandle);
-  }
-  
-  if (bitmapDeviceContext == NULL) {
-    bitmapDeviceContext = CreateCompatibleDC(NULL);
-  }
+  bitmapWidth = width;
+  bitmapHeight = height;
 
   bitmapInfo = (BITMAPINFO){
       .bmiHeader =
           {
               .biSize = sizeof(BITMAPINFOHEADER),
               .biWidth = width,
-              .biHeight = height,
+              .biHeight = -height,  // top-down image
               .biPlanes = 1,
-              .biBitCount = 32,
+              .biBitCount = bytesPerPixel * 8,
               .biCompression = BI_RGB,
           },
   };
-  bitmapHandle = CreateDIBSection(bitmapDeviceContext, &bitmapInfo,
-                                  DIB_RGB_COLORS, &bitmapMemory, NULL, 0);
+
+  SIZE_T bitmapMemorySize = ((SIZE_T)width * (SIZE_T)height * bytesPerPixel);
+  bitmapMemory =
+      VirtualAlloc(NULL, bitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+  if (bitmapMemory == NULL) {
+    OutputDebugStringA("Error:VirtualAlloc");
+    return;
+  }
+
+  // renderStuff();
 }
 
-internal void Win32UpdateWindow(HDC deviceContext, int x, int y, int width,
-                                int height) {
-  StretchDIBits(deviceContext, x, y, width, height, x, y, width, height,
-                bitmapMemory, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+internal void Win32UpdateWindow(HDC deviceContext, RECT windowRect) {
+  LONG windowWidth = windowRect.right - windowRect.left;
+  LONG windowHeight = windowRect.bottom - windowRect.top;
+  StretchDIBits(deviceContext,
+                // destination rectangle
+                0, 0, bitmapWidth, bitmapHeight,
+                // source rectangle
+                0, 0, windowWidth, windowHeight, bitmapMemory, &bitmapInfo,
+                DIB_RGB_COLORS, SRCCOPY);
 }
